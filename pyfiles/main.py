0e34646afe4d9b7860f682e994a2f6a3659a2141
93mmm <119791265+93mmm@users.noreply.github.com>
2022-12-23 20:02:14 +0300

    Update main.py

--- pyfiles/main.py
+++ pyfiles/main.py
@@ -1,1 +1,24 @@
+import pygame
+import json
+import sys
 
+from game import Game
+
+def check_event(event):
+    pass
+
+with open("json_files/settings.json", "r") as file:
+    data = json.load(file)
+
+pygame.init()
+screen_width, screen_height = data["window_size"]
+screen = pygame.display.set_mode((screen_width,screen_height))
+clock = pygame.time.Clock()
+game = Game()
+
+while True:
+    for e in pygame.event.get():
+        check_event(e)
+    screen.fill('grey')
+    pygame.display.update()
+    clock.tick(60)
